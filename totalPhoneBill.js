e46d3a28dc47f2fc706c73521b7b0415170940c3
Kopanomasuku23 <kopanomasuku@gmail.com>
2023-08-28 12:32:23 +0200

    first commit

---
+++ totalPhoneBill.js
@@ -0,0 +1,26 @@
+export function totalPhoneBill(bill) {
+  let call = 2.75;
+  let sms = 0.65;
+  let callCount = 0;
+  let smsCount = 0;
+
+  let billArr = bill.split(",");
+
+  //loop through the new array containing the calls adnd SMSs
+  for (let i in billArr) {
+    let trimmed = billArr[i].trim();
+
+    if (trimmed.includes("call")) {
+      callCount += 1;
+
+    } else if (trimmed.includes("sms")) {
+      smsCount += 1;
+    }
+  }
+
+
+  let total = callCount * call + smsCount * sms;
+
+  return "R" + total.toFixed(2);
+}
+console.log(totalPhoneBill('call, sms, call, sms, sms'))
